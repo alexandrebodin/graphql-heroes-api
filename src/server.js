eda817cbf26c3c6a832f01f2940ef67ad18a061e
Alexandre BODIN <bodin.alex@gmail.com>
2017-09-11 16:28:47 +0200

    working on different domains

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const apiHandler = require('./api')
+const cors = require('cors')
 
 const server = express()
 
-server.use('/api', apiHandler)
+server.use(cors())
+server.use(apiHandler)
 
-server.listen(3001, err => {
+server.listen(3002, err => {
   if (err) throw err
-  console.log('> Ready on http://localhost:3001')
+  console.log('> Ready on http://localhost:3002')
 })
